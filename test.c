023bce6a72cc2d937d0fb3654bc3af63d2c74b5a
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2023-01-12 00:01:39 +1100

    More updates on test.c

--- test.c
+++ test.c
@@ -5,6 +5,20 @@ int main() {
 
 	printf("Hello how are you?\n");
 
+	printf("Please enter your name: ");
+	
+	char name[50];
+
+	scanf("%s", name);
+
+	printf("Please enter your age: ");
+
+
+	int age;
+	scanf("%d", &age);
+
+	printf("Your name is %s and you are %d years old\n", name, age);
+
  	return 0;
 
 }
